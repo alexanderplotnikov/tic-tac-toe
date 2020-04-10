ea8583f436cce4d09d2d3121ed9a7e6ba490994c
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-04-10 20:56:42 +0300

    improve functionality

--- main.js
+++ main.js
@@ -2,31 +2,64 @@
 
 //gameBoard
 const gameBoard = (function (document){
-    const selection = ['', '', '', '', '', '', '', '', ''];
+    const selection = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const getSelection = () => selection;
     const mainNode = document.querySelector(".container");
     const makeGrid = () => {
         let j = 0;
         for(let i = 0; i < 9; i++){
             let cell = document.createElement("DIV");
             let mark = '';
-            
-            cell.addEventListener("click", () => {
+            let updateGrid = () => {
                 j % 2 == 0 ? mark = "x" : mark = "o";
                 selection.splice(i, 1, mark); 
                 cell.innerHTML = selection[i];
-                this.removeEventListener("click", this.cell);
                 j++;
-            })
+                cell.removeEventListener("click", updateGrid);
+                isWinner();
+            }
+            cell.addEventListener("click", updateGrid);
             mainNode.appendChild(cell);
         }
     }  
+    let turnCount = 9;
+    const isWinner = () => {
+            turnCount--;
+            if (selection[0] == selection[1] && selection[1] == selection[2]){
+                return alert("Player won");
+            }
+            else if(selection[3] == selection[4] && selection[4] == selection[5]){
+                return alert("Player won");
+            }
+            else if(selection[6] == selection[7] && selection[7] == selection[8]){
+                return alert("Player won");
+            }
+            else if(selection[0] == selection[3] && selection[3] == selection[6]){
+                return alert("Player won");
+            }
+            else if(selection[1] == selection[4] && selection[4] == selection[7]){
+                return alert("Player won");
+            }
+            else if(selection[2] == selection[5] && selection[5] == selection[8]){
+                return alert("Player won");
+            }
+            else if(selection[0] == selection[4] && selection[4] == selection[8]){
+                return alert("Player won");
+            }
+            else if(selection[2] == selection[4] && selection[4] == selection[6]){
+                return alert("Player won");
+            }
+            else if(turnCount == 0){
+                return alert("Tie");
+            }        
+    };
     return {
-        makeGrid,
+        makeGrid, getSelection,
     }
-})(document);
+})(document);1
 
 //displayController 
-const displayController = (function (){
+const displayController = (function (){ 
     gameBoard.makeGrid();
 })();
 
