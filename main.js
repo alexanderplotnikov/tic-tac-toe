4a73fa8d48ea6f56ff7a625954f8156af7f6aecc
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-05-07 23:28:55 +0300

    factory functions; functionality complete

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-//Modules
-
+ //Modules
 //gameBoard
-const gameBoard = (function (document){
-    const selection = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let mainNode = document.querySelector(".container");
+const gameBoard = (function (){
+    let selection = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const getSelection = () => selection;
-    const mainNode = document.querySelector(".container");
+    
     const makeGrid = () => {
         let j = 0;
         for(let i = 0; i < 9; i++){
@@ -21,48 +21,70 @@ const gameBoard = (function (document){
             cell.addEventListener("click", updateGrid);
             mainNode.appendChild(cell);
         }
-    }  
+    }
+    const resetBtn = document.getElementById("resetBtn");
+    resetBtn.addEventListener("click", () => {
+        resetGrid();
+        player1 = {};
+        player2 = {};
+        mainNode.classList.add("hidden");
+        newGameBtn.classList.toggle("hidden");
+        resetBtn.classList.toggle("hidden");
+    })
+    const resetGrid = () => {
+        selection = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        mainNode.innerHTML = "";
+        turnCount = 9;
+        makeGrid();
+    }
+    function winMessage(mark) {
+        let name;
+        player1.getMark() == mark ? name = player1.getName() : name = player2.getName();
+        alert(`${name} won this round`);
+        resetGrid();
+    }
     let turnCount = 9;
+    
     const isWinner = () => {
             turnCount--;
             if (selection[0] == selection[1] && selection[1] == selection[2]){
-                return alert("Player won");
+                winMessage(selection[0]);
             }
             else if(selection[3] == selection[4] && selection[4] == selection[5]){
-                return alert("Player won");
+                winMessage(selection[3]);
             }
             else if(selection[6] == selection[7] && selection[7] == selection[8]){
-                return alert("Player won");
+                winMessage(selection[6]);
             }
             else if(selection[0] == selection[3] && selection[3] == selection[6]){
-                return alert("Player won");
+                winMessage(selection[0]);
             }
             else if(selection[1] == selection[4] && selection[4] == selection[7]){
-                return alert("Player won");
+                winMessage(selection[1]);
             }
             else if(selection[2] == selection[5] && selection[5] == selection[8]){
-                return alert("Player won");
+                winMessage(selection[2]);
             }
             else if(selection[0] == selection[4] && selection[4] == selection[8]){
-                return alert("Player won");
+                winMessage(selection[0]);
             }
             else if(selection[2] == selection[4] && selection[4] == selection[6]){
-                return alert("Player won");
+                winMessage(selection[2]);
             }
             else if(turnCount == 0){
+                resetGrid();
                 return alert("Tie");
             }        
     };
     return {
         makeGrid, getSelection,
     }
-})(document);1
+})();
 
 //displayController 
 const displayController = (function (){ 
     gameBoard.makeGrid();
-})();
-
+})(); 
 //Factory Function
 const Player = (name, mark) => {
     const getMark = () => mark;
@@ -72,5 +94,21 @@ const Player = (name, mark) => {
     }
 };
 
-let player1 = Player("John", "x");
-let player2 = Player("Wick", "o");
+const newGameBtn = document.getElementById("newGameBtn");
+const playerOne = document.getElementById("playerOne");
+const playerTwo = document.getElementById("playerTwo");
+let player1;
+let player2;
+newGameBtn.addEventListener("click", () => {
+    let randNum = Math.floor(Math.random() * 2);
+    randNum == 1 ? markOne = "x": markOne = "o";
+    randNum == 1 ? markTwo = "o": markTwo = "x";
+    if(mainNode.classList.contains("hidden")){
+        mainNode.classList.remove("hidden");
+    }
+    newGameBtn.classList.toggle("hidden");
+    resetBtn.classList.toggle("hidden");
+    player1 = Player(playerOne.value, markOne);
+    player2 = Player(playerTwo.value, markTwo);
+    return player1, player2; 
+});
